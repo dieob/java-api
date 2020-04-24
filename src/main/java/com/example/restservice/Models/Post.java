9a9ac0301d27659aa7a8e0a92009388bb1076722
dieob <dbaezrenault@gmail.com>
2020-04-24 12:49:38 -0400

    Added configuration for Heroku Postgres

--- src/main/java/com/example/restservice/Models/Post.java
+++ src/main/java/com/example/restservice/Models/Post.java
@@ -1,5 +1,6 @@
 package com.example.restservice.Models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,15 +18,22 @@ public class Post {
     }
     
 
-    public Post(String description, String title, String city) {
+    public Post(String title, String description, String city) {
         this.description = description;
         this.title = title;
+        this.city = city;
     }
 
     @Id
     @GeneratedValue
     private Long id;
+    
+    @Column
     private String title;
+    
+    @Column
     private String description;
+    
+    @Column
     private String city;
 }
--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.restservice;
 
+import com.example.restservice.Models.Post;
+import com.example.restservice.Repository.PostRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestServiceApplication {
@@ -9,4 +13,12 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
+        
+        @Bean
+        ApplicationRunner applicationRunner(PostRepository postRepository){
+            return args -> {
+                postRepository.save(new Post("Title1", "Desc1", "City1"));
+                postRepository.save(new Post("Title2", "Post2", "City2"));
+            };
+        }
 }
